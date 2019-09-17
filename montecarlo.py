dc2b50c62954df6352e033119d695b93e71276f5
Oscar Serna <oscar.eduardo.serna@gmail.com>
2019-09-17 11:21:38 -0500

    Dashboard using panel ready. waiting for user input in dict form

--- montecarlo.py
+++ montecarlo.py
@@ -79,35 +79,64 @@ def select_tickers():
 def read_json(url):
     request = Request(url)
     response = urlopen(request)
+    #print(response)
     data = response.read()
+    #print(data)
     url2 = json.loads(data)
     return url2
 
 def get_crypto_daily_price(cryptotickers = [], allData=False,limit = 90):
-
+    """
+    Returns a dataframe with the close prices of the cryptocurrencies selected. 
+    Arguments:
+    cryptotickers: list of tickers for currencies.
+    allData: if True, gets all historical data available and ignores argument "limit".
+    By default it is False.
+    limit: the days from now to get the historical data. By default it's 90.
+    """
     api_key = os.getenv("CC_API")
     ticker_list = cryptotickers
     crypto_df = pd.DataFrame()
 
     for ticker in ticker_list:
+        #if allData is true, then it gets all the data available. If not, select data according to limit.
         if allData:
             url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym={ticker}&tsym=USD&allData=true&api_key={api_key}"
         else:
             url = f"https://min-api.cryptocompare.com/data/v2/histoday?fsym={ticker}&tsym=USD&limit={limit}&api_key={api_key}"
        
         raw_data = read_json(url)
+        #print(json.dumps(raw_data, indent=5))
         df = pd.DataFrame(raw_data['Data']['Data'])
         df['time'] = pd.to_datetime(df['time'],unit='s')
         df.set_index(df['time'], inplace=True)
         df['close'] = df['close'].astype(float)
         crypto_df[ticker] = df['close']
-
+    
+    #
     new_columns = pd.MultiIndex.from_product([ crypto_df.columns, ["close"]  ])
     crypto_df.columns = new_columns
 
     return crypto_df
     
 ### END CRHISTIAN CODE######
+
+def normalize_dataframe(df=None):
+    """
+    Returns a dataframe with the normalize format for this library.
+    The normalize format is a dataframe with only the close values of the ticker with the 
+    columns named as the ticker. 
+    Accepts dataframes with columns that are 2-level indexed where the upper index contains
+    the ticker and the lower has at least one column named "close".
+    """
+    #Drops all the columns that are not 'close'
+    col_to_drop = df.columns.levels[1].values
+    col_to_drop = np.delete(col_to_drop,np.where(col_to_drop=="close"))
+    df = df.drop(columns=col_to_drop, level=1)
+    #eliminates the double level in columns by deleting the 'close' label. 
+    #Only ticker label is necessary.
+    df.columns = df.columns.droplevel(1)
+    return df
 
 def get_historic_data(end_date = datetime.now(), 
                       start_date = datetime.now() + timedelta(-365),
@@ -185,14 +214,8 @@ def monte_carlo_sim(df=None, trials=1000, sim_days=252,weights=[]):
     ##has the HOLC values and one of the columns has the name "close".
     
     #Dataframe formatting:
-    
-    #Drops all the columns that are not 'close'
-    col_to_drop = df.columns.levels[1].values
-    col_to_drop = np.delete(col_to_drop,np.where(col_to_drop=="close"))
-    df = df.drop(columns=col_to_drop, level=1)
-    #eliminates the double level in columns by deleting the 'close' label. 
-    #Only ticker label is "necessary".
-    df.columns = df.columns.droplevel(1)
+    if(df.columns.nlevels>1):
+        df = normalize_dataframe(df)
     daily_rteurns = df.pct_change().copy()
     
     #checks if the weights provided are correct:
