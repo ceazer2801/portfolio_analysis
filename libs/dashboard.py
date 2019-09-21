7c3c54f27474a219eedbb74a1517ea6428603603
Christian Cathro <cscathro@gmail.com>
2019-09-21 16:57:55 -0500

    Added the TA functions to dashboard.py, although commented out until drop down selection in place to prevent breaking

--- libs/dashboard.py
+++ libs/dashboard.py
@@ -249,6 +249,46 @@ def plot_conf(values=None,conf=[0,0]):
     plt.close()
     return pn.Pane(conifidence_plot)
 
+def bb_plot(df):
+    bb_plot = plt.figure(figsize=(12,8));
+    plt.plot(df.close)
+    plt.plot(df.volatility_bbh, label='High BB')
+    plt.plot(df.volatility_bbl, label='Low BB')
+    plt.plot(df.volatility_bbm, label='EMA BB')
+    plt.title('Bollinger Bands')
+    plt.legend()
+    plt.close()
+    return pn.Pane(bb_plot)
+
+def ichi_plot(df):
+    ichi_plot = plt.figure(figsize=(12,8));
+    plt.plot(df.close)
+    plt.plot(df.trend_ichimoku_a, label='Ichimoku a')
+    plt.plot(df.trend_ichimoku_b, label='Ichimoku b')
+    plt.title('Ichimoku Kinko Hyo')
+    plt.legend()
+    plt.close()
+    return pn.Pane(ichi_plot)
+
+def ema_plot(df):
+    ema_plot = plt.figure(figsize=(12,8));
+    plt.plot(df.close)
+    plt.plot(df.volatility_bbm, label='EMA BB')
+    plt.title('Exponential Moving Average')
+    plt.legend()
+    plt.close()
+    return pn.Pane(ema_plot)
+
+def macd_plot(df):
+    macd_plot = plt.figure(figsize=(12,8));
+    plt.plot(df.trend_macd, label='MACD');
+    plt.plot(df.trend_macd_signal, label='MACD Signal')
+    plt.plot(df.trend_macd_diff, label='MACD Difference')
+    plt.title('MACD, MACD Signal and MACD Difference')
+    plt.legend()
+    plt.close()
+    return pn.Pane(macd_plot)
+
 
 def get_dashboard(tickers_dict={"index":[],"crypto":[]}, years=2, mc_trials=500, mc_sim_days=252, weights=None):
     
@@ -275,9 +315,11 @@ def get_dashboard(tickers_dict={"index":[],"crypto":[]}, years=2, mc_trials=500,
     )
 
     techl_analysis_tabs = pn.Tabs(
-        ("TA1","in construction"),
-        ("TA2", "in construction"),
-        #background="whitesmoke"
+#        ("Exp. Moving Avg.",ema_plot(ta_df)),
+#        ("Bollinger Bands", bb_plot(ta_df)),
+#        ("MACD",macd_plot(ta_df)),
+ #       ("Ichimoku Kinkō Hyō", ichi_plot(ta_df)),
+    #    background="whitesmoke"
     )
 
     tabs = pn.Tabs(
